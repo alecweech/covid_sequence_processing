cccc8b383de659b2290a4261a5554ca28fdf3797
alecweech <aaweech@email.wm.edu>
2021-09-24 15:20:15 -0400

    cleaned mutations data

---
+++ variant_loader.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import glob
+import pandas as pd
+import re
+
+#Get csv files from path
+path = os.getcwd()
+path += '\covid_sequence_processing\MutationSummary'
+csv_files = glob.glob(os.path.join(path, '*.csv'))
+print(csv_files)
+mutations = pd.DataFrame()
+
+for file in csv_files:
+    df = pd.read_csv(file)
+    mutations = pd.concat([mutations, df])
+
+mutations[['nucleotide', 'change']] = mutations['mutation_site'].str.replace(r'\'', '').str.split(':', expand = True)
+mutations[['WT', 'SNP']] = mutations['change'].str.split('->', expand = True)
+mutations[['WT_AA', 'SNP_AA']] = mutations['protein_site(actual)'].str.split(r"[^A-Z]+", expand = True)
+mutations = mutations.drop(['mutation_site', 'protein_site(actual)', 'change'], axis = 'columns')
+
+
+mutations.to_csv('cleaned_mutations.csv')
